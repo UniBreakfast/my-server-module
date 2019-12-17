bffbb934267fc6671d76c7a2c3db49b41f1d60ee
unibreakfast <unibreakfast1@gmail.com>
2019-12-17 14:39:41 +0200

    flowchart added and handlereq changed accordingly

--- handlereq.js
+++ handlereq.js
@@ -1,20 +1,22 @@
 const fs = require('fs'), fsp = fs.promises
 
 module.exports = async (req, resp, dev)=> {
-  const { api } = req.socket._server
+  const { apiClerk } = req.socket._server
 
   let { url } = req
 
-  if (api && url.startsWith('/api/')) {
-    const reqBody = (parts=[])=> new Promise((resolve, reject)=>
-      req.on('error', reject).on('data', part => parts.push(part))
-        .on('end', ()=> resolve(Buffer.concat(parts).toString('utf8'))))
-    try {
-      const data = await api(req, JSON.parse(await reqBody()), dev)
-      resp.setHeader('Content-Type', 'application/json')
-      return resp.end(JSON.stringify(data))
-    }
-    catch (e) { console.log('api error:', e) }
+  if (apiClerk && url.startsWith('/api/')) {
+    // const reqBody = (parts=[])=> new Promise((resolve, reject)=>
+    //   req.on('error', reject).on('data', part => parts.push(part))
+    //     .on('end', ()=> resolve(Buffer.concat(parts).toString('utf8'))))
+    // try {
+    //   const data = await apiClerk(req, JSON.parse(await reqBody() || '{}'), dev)
+    //   resp.setHeader('Content-Type', 'application/json')
+    //   return resp.end(JSON.stringify(data))
+    // }
+    // catch (e) { console.log('api error:', e) }
+    resp.setHeader('Content-Type', 'application/json')
+    return resp.end(JSON.stringify(apiClerk.handle(req)))
   }
 
   if (url=='/favicon.ico' && dev) url = '/devicon.ico'
@@ -22,10 +24,10 @@ module.exports = async (req, resp, dev)=> {
   let path = process.cwd()+'/public'+url
 
   try {
-    const target = await fsp.stat(path).catch(_=> fsp.stat(path+'.html'))
-    if (target.isDirectory()) path += '/index'
+    const target = await fsp.stat(path).catch(_=> fsp.stat(path+='.html'))
+    if (target.isDirectory()) path += '/index.html'
     const match = path.match(/\.(\w+)$/), ext = match? match[1] : 'html'
-    if (!match) path += '.html'
+    // if (!match) path += '.html'
 
     fs.createReadStream(path).pipe(resp)
 
